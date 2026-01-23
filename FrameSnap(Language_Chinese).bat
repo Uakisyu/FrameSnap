c84336ebd13af24d3191f4e4e7ee3e1a082999b3
荼珂珂💫 <itu@Outlook.ie>
2026-01-23 14:59:58 +0800

    Add files via upload

---
+++ FrameSnap(Language_Chinese).bat
@@ -0,0 +1,99 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+color 2
+title ※ 视频一键抽帧 · By iTufke ※
+
+echo ==== 视频一键抽帧 By iTufke ====
+echo .                              .
+echo   *** 使用 FFmpeg 实现抽帧 ***
+echo .                              .
+echo ================================
+echo.
+
+rem 检测ffmpeg.exe是否存在
+set "FFMPEG=%~dp0ffmpeg.exe"
+if not exist "%FFMPEG%" (
+  echo [Tuf.Bat] 错误, 当前目录没有找到ffmpeg.exe文件.
+  echo [Tuf.Bat] 请将ffmpeg.exe放入当前运行目录.
+  echo.
+  pause
+  exit /b 1
+)
+
+rem 让用户输入视频名称/路径
+set "videopath="
+set /p videopath=请输入要被抽帧的视频 (文件名或详细路径, 视频格式后缀名结尾): 
+echo.
+
+rem 判断用户输入
+if "%videopath%"=="" (
+  echo [Tuf.Bat] 错误, 你没有输入任何内容.
+  echo.
+  pause
+  exit /b 1
+)
+
+rem 判断用户输入的视频是否存在 
+if not exist "%videopath%" (
+  echo [Tuf.Bat] 错误, 找不到你输入的视频文件: "%videopath%".
+  pause
+  exit /b 1
+)
+
+rem 取视频名称并设置为输出文件夹名
+for %%F in ("%videopath%") do (
+  set "name=%%~nF"
+)
+set "outdir=!name!"
+
+rem 让用户输入抽帧速率
+set "fps="
+set /p fps=请输入抽帧速率 (每秒几张，默认 5): 
+
+rem 判断用户输入是否为空, 如是则启用默认值
+if "%fps%"=="" (
+  echo.
+  echo [Tuf.Bat] 错误, 你没有输入任何内容, 已默认为每秒抽 5 张.
+  set "fps=5"
+)
+
+rem 判断用户输入是否为纯数字, 如不是则启用默认值
+for /f "delims=0123456789" %%A in ("%fps%") do (
+  echo.
+  echo [Tuf.Bat] 错误, 你输入的速率并不是纯数字, 已默认为每秒抽 5 张.
+  set "fps=5"
+)
+
+rem 为用户显示抽帧输出信息
+echo.
+echo [Tuf.Bat] 成功, 已找到指定视频, 请确认以下输出信息:
+echo.
+echo 视频输入: "%videopath%"
+echo 抽帧输出目录: "!outdir!"
+echo 抽帧设定：每秒 !fps! 张
+echo.
+echo [Tuf.Bat] 按任意键开始抽帧.
+echo.
+pause
+
+rem 创建对应文件夹
+if not exist "!outdir!" (
+  mkdir "!outdir!"
+)
+
+rem 开始抽帧
+ffmpeg -hide_banner -y -i "%videopath%" -vf "fps=!fps!" -q:v 2 "!outdir!\!outdir!_%%06d.jpg"
+
+rem 显示抽帧完成后信息
+cls
+echo ==== 视频一键抽帧 By iTufke ===
+echo .                             .
+echo   *** 使用 FFmpeg 实现抽帧 ***
+echo .                             .
+echo ===============================
+echo.
+echo [Tuf.Bat] 抽帧已完成, 所有结果已保存到本地的 "!outdir!" 文件夹.
+echo.
+echo [Tuf.Bat] 按任意键退出脚本
+pause
+endlocal
---
+++ FrameSnap(Language_English).bat
@@ -0,0 +1,99 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+color 2
+title ¡ù FrameSnap ¡¤ By iTufke ¡ù
+
+echo ==== FrameSnap By iTufke ====
+echo .                           .
+echo    **** Using FFmpeg  ****
+echo .                           .
+echo =============================
+echo.
+
+rem Check if ffmpeg.exe exists
+set "FFMPEG=%~dp0ffmpeg.exe"
+if not exist "%FFMPEG%" (
+  echo [Tuf.Bat] Error, ffmpeg.exe not found.
+  echo [Tuf.Bat] Place ffmpeg.exe in the current running directory.
+  echo.
+  pause
+  exit /b 1
+)
+
+rem Ask the user to input the video name/path
+set "videopath="
+set /p videopath=Input video Name or Path: 
+echo.
+
+rem If user input is empty
+if "%videopath%"=="" (
+  echo [Tuf.Bat] Error, the input is empty.
+  echo.
+  pause
+  exit /b 1
+)
+
+rem If the video input by the user exists
+if not exist "%videopath%" (
+  echo [Tuf.Bat] Error, video not found: "%videopath%".
+  pause
+  exit /b 1
+)
+
+rem Retrieve the video name and set it as the output folder name.
+for %%F in ("%videopath%") do (
+  set "name=%%~nF"
+)
+set "outdir=!name!"
+
+rem Ask the user to input the frame rate
+set "fps="
+set /p fps=Input the frame rate (Frame rate per second, Default 5): 
+
+rem If the user input is empty; if so, use the default.
+if "%fps%"=="" (
+  echo.
+  echo [Tuf.Bat] Error, the input is empty. Used default value is 5.
+  set "fps=5"
+)
+
+rem if the user input is purely numeric; if not, use the default.
+for /f "delims=0123456789" %%A in ("%fps%") do (
+  echo.
+  echo [Tuf.Bat] Error, the input not a pure number. Used default value is 5.
+  set "fps=5"
+)
+
+rem Display settings
+echo.
+echo [Tuf.Bat] Confirm the settings info:
+echo.
+echo Video: "%videopath%"
+echo Output folder: "!outdir!"
+echo Frame rate£º !fps! frame per second
+echo.
+echo [Tuf.Bat] Press Anykey to start FrameSnap.
+echo.
+pause
+
+rem Create the folder
+if not exist "!outdir!" (
+  mkdir "!outdir!"
+)
+
+rem Run ffmpeg.exe
+ffmpeg -hide_banner -y -i "%videopath%" -vf "fps=!fps!" -q:v 2 "!outdir!\!outdir!_%%06d.jpg"
+
+rem Complete
+cls
+echo ==== FrameSnap By iTufke ====
+echo .                           .
+echo    **** Using FFmpeg  ****
+echo .                           .
+echo =============================
+echo.
+echo [Tuf.Bat] Complete, and all results have been saved to the "!outdir!" folder.
+echo.
+echo [Tuf.Bat] Press Anykey to exit.
+pause
+endlocal
