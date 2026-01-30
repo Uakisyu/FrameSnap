1c6a412f789bfb42d1bb74e44dc8b799f289e7c5
秋柚💫 <uak@outlook.ie>
2026-01-30 19:01:57 +0800

    将旧用户名"iTufke"更改为新用户名"Uakisyu"

--- FrameSnap(Language_Chinese).bat
+++ FrameSnap(Language_Chinese).bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 color 2
-title ※ 视频一键抽帧 · By iTufke ※
+title ※ 视频一键抽帧 · By Uakisyu ※
 
-echo ==== 视频一键抽帧 By iTufke ====
+echo ==== 视频一键抽帧 By Uakisyu ====
 echo .                              .
 echo   *** 使用 FFmpeg 实现抽帧 ***
 echo .                              .
@@ -13,8 +13,8 @@ echo.
 rem 检测ffmpeg.exe是否存在
 set "FFMPEG=%~dp0ffmpeg.exe"
 if not exist "%FFMPEG%" (
-  echo [Tuf.Bat] 错误, 当前目录没有找到ffmpeg.exe文件.
-  echo [Tuf.Bat] 请将ffmpeg.exe放入当前运行目录.
+  echo [Uak.Bat] 错误, 当前目录没有找到ffmpeg.exe文件.
+  echo [Uak.Bat] 请将ffmpeg.exe放入当前运行目录.
   echo.
   pause
   exit /b 1
@@ -27,7 +27,7 @@ echo.
 
 rem 判断用户输入
 if "%videopath%"=="" (
-  echo [Tuf.Bat] 错误, 你没有输入任何内容.
+  echo [Uak.Bat] 错误, 你没有输入任何内容.
   echo.
   pause
   exit /b 1
@@ -35,7 +35,7 @@ if "%videopath%"=="" (
 
 rem 判断用户输入的视频是否存在 
 if not exist "%videopath%" (
-  echo [Tuf.Bat] 错误, 找不到你输入的视频文件: "%videopath%".
+  echo [Uak.Bat] 错误, 找不到你输入的视频文件: "%videopath%".
   pause
   exit /b 1
 )
@@ -53,26 +53,26 @@ set /p fps=请输入抽帧速率 (每秒几张，默认 5):
 rem 判断用户输入是否为空, 如是则启用默认值
 if "%fps%"=="" (
   echo.
-  echo [Tuf.Bat] 错误, 你没有输入任何内容, 已默认为每秒抽 5 张.
+  echo [Uak.Bat] 错误, 你没有输入任何内容, 已默认为每秒抽 5 张.
   set "fps=5"
 )
 
 rem 判断用户输入是否为纯数字, 如不是则启用默认值
 for /f "delims=0123456789" %%A in ("%fps%") do (
   echo.
-  echo [Tuf.Bat] 错误, 你输入的速率并不是纯数字, 已默认为每秒抽 5 张.
+  echo [Uak.Bat] 错误, 你输入的速率并不是纯数字, 已默认为每秒抽 5 张.
   set "fps=5"
 )
 
 rem 为用户显示抽帧输出信息
 echo.
-echo [Tuf.Bat] 成功, 已找到指定视频, 请确认以下输出信息:
+echo [Uak.Bat] 成功, 已找到指定视频, 请确认以下输出信息:
 echo.
 echo 视频输入: "%videopath%"
 echo 抽帧输出目录: "!outdir!"
 echo 抽帧设定：每秒 !fps! 张
 echo.
-echo [Tuf.Bat] 按任意键开始抽帧.
+echo [Uak.Bat] 按任意键开始抽帧.
 echo.
 pause
 
@@ -86,14 +86,14 @@ ffmpeg -hide_banner -y -i "%videopath%" -vf "fps=!fps!" -q:v 2 "!outdir!\!outdir
 
 rem 显示抽帧完成后信息
 cls
-echo ==== 视频一键抽帧 By iTufke ===
+echo ==== 视频一键抽帧 By Uakisyu ===
 echo .                             .
 echo   *** 使用 FFmpeg 实现抽帧 ***
 echo .                             .
 echo ===============================
 echo.
-echo [Tuf.Bat] 抽帧已完成, 所有结果已保存到本地的 "!outdir!" 文件夹.
+echo [Uak.Bat] 抽帧已完成, 所有结果已保存到本地的 "!outdir!" 文件夹.
 echo.
-echo [Tuf.Bat] 按任意键退出脚本
+echo [Uak.Bat] 按任意键退出脚本
 pause
 endlocal
